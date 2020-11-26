06862be0c49ab28fbf27d31cdff391ed0daa3983
abhijay-pradhan <73331596+abhijay-pradhan@users.noreply.github.com>
2020-11-26 23:05:17 +0530

    Update main.js

--- main.js
+++ main.js
@@ -4,3 +4,6 @@ window.open("https://www.youtube.com/channel/UCVlfq8p7HP5rV06rWdPQ_JA")
 function em() {
 window.open("mailto:abhijayprs12@gmail.com")
 }
+function jml() {
+	window.open("https://thunkable.site/w/d9m4lnBjh")
+}
